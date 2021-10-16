ac5eac60afeff1e2bff33facf73468c5248af32e
Mekki Nada <44327249+mekkinada@users.noreply.github.com>
2021-10-16 09:24:18 +0100

    Add files via upload

---
+++ config/environment/development.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //9000
+  port: 3000,
+  mongoURI: 'mongodb+srv://authentication:authentication@cluster0.bkvds.mongodb.net/cards?retryWrites=true&w=majority',
+  logLevel: 'debug',
+  secret: 'keySecretForDevelopment',
+};
